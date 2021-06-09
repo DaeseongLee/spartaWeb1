a1fbfffc9625be55f42920fb8b2a36aeec182dcb
DaeseongLee <hsl5539@gmail.com>
2021-06-09 16:49:54 +0900

    Change main UI

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ def auth(msg):
         print(user)
         if user == {}:
             return render_template("login.html")
+        
         else:
             return render_template('main.html',  data=json.dumps(user), msg=msg, user=user)
     except Exception as e:
